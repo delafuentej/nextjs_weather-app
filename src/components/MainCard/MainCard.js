0a483b1eb3bb59475ff7152e074d8413b4d1f274
delafuentej <jfuentelucas@gmail.com>
2024-05-04 12:29:28 +0200

    maincard component structured + styles

--- src/components/MainCard/MainCard.js
+++ src/components/MainCard/MainCard.js
@@ -1,9 +1,24 @@
-import './MainCard.module.css';
+import Image from 'next/image';
+import {celsiusToFahrenheit} from '../../../utils/converters';
+import styles from'./MainCard.module.css';
 
-export const MainCard=()=>{
+
+
+ export const MainCard=()=>{
     return(
-        <div>
-            MainCard
+        <div className={styles.wrapper}>
+            <h1 className={styles.location}>
+                city, country
+            </h1>
+
+            <p className={styles.description}> description weather</p>
+
+            <Image /> 
+
+            <h2 className={styles.temperatur}> °C/F</h2>
+
+            <p>Feels like</p>
         </div>
     )
 };
+
