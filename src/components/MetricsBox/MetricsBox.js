289bd8129681a83f8a5488e7d8b36a104a054838
delafuentej <jfuentelucas@gmail.com>
2024-05-04 15:46:33 +0200

    component unitswitch estructured + styles.modules

--- src/components/MetricsBox/MetricsBox.js
+++ src/components/MetricsBox/MetricsBox.js
@@ -1,9 +1,25 @@
-import './MetricsBox.module.css';
+import { MetricsCard } from '../MetricsCard/MetricsCard';
+import styles from './MetricsBox.module.css';
 
 export const MetricsBox=()=>{
     return(
-        <div>
-            MetricsBox
+        <div className ={styles.wrapper}>
+           <MetricsCard 
+            title={'Humidity'}
+           />
+           <MetricsCard
+            title={'Wind_Speed'}
+           />
+           <MetricsCard 
+            title={'Wind_Direction'}
+           />
+           <MetricsCard 
+            title={'Visibility'}
+           />
+           <MetricsCard 
+            title={'Sunrise'}/>
+           <MetricsCard 
+            title={'Sunset'} />
         </div>
     )
 };
--- src/components/MetricsCard/MetricsCard.js
+++ src/components/MetricsCard/MetricsCard.js
@@ -1,9 +1,18 @@
-import './MetricsCard.module.css';
+import Image from 'next/image';
+import styles from './MetricsCard.module.css';
 
 export const MetricsCard=()=>{
     return(
-        <div>
-            MetricsCard
+        <div styles={styles.wrapper}>
+            <h1>Title</h1>
+            <div className={styles.content}>
+                <Image />
+                <div>
+                    <h3>Metric</h3>
+                    <p>Unit</p>
+                </div>
+
+            </div>
         </div>
-    )
+    );
 };
