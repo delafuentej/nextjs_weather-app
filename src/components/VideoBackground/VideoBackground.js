501d465c2092c6d7cac760ffe99912d405a5e94a
delafuentej <jfuentelucas@gmail.com>
2024-05-12 20:06:32 +0200

    video-background atmospheric phenomena added: to be optimised

--- src/components/VideoBackground/VideoBackground.js
+++ src/components/VideoBackground/VideoBackground.js
@@ -33,7 +33,7 @@ import styles from './VideoBackground.module.css';
 export const VideoBackground =()=>{
     return(
         <div className={styles.VideoBackground}>
-            <video src={require('../../../public/videos/sunSee.mp4')} autoPlay muted loop className={styles.video}/>
+            <video src={require('../../../public/videos/video-background.mp4')} autoPlay muted loop className={styles.video}/>
         </div>
     )
 }
