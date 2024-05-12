9cda212c80210a54cebef9a9a2f1270e70e5d1be
delafuentej <jfuentelucas@gmail.com>
2024-05-12 11:38:20 +0200

    styles components changed

--- src/components/MetricsCard/MetricsCard.js
+++ src/components/MetricsCard/MetricsCard.js
@@ -8,8 +8,8 @@ export const MetricsCard=({ title, iconSrc, metric, unit})=>{
             <h2>{title}</h2>
             <div className={styles.content}>
                 <Image 
-                    width='100'
-                    height='100'
+                    width='75'
+                    height='75'
                     src = {iconSrc}
                     alt = 'weatherIcon'
                 
