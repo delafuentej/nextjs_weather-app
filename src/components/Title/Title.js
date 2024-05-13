750b7bf1dc5532c4088d9311b9ae0688742e7ed5
delafuentej <jfuentelucas@gmail.com>
2024-05-13 12:12:14 +0200

    error screen completed, pending responsive design with mediaqueries

---
+++ src/components/Title/Title.js
@@ -0,0 +1,11 @@
+import styles from './Title.module.css';
+
+export const Title = ()=>{
+    return (
+        <div className={styles.title}>
+
+            <h1>Weather API  </h1>
+   
+        </div>
+    )
+}
