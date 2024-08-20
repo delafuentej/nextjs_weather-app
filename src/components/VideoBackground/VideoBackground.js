0a321f6e902c05ade86f7a74458ba70a9c314702
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-08-20 15:41:47 +0200

    responsive design: changes III

--- src/components/VideoBackground/VideoBackground.js
+++ src/components/VideoBackground/VideoBackground.js
@@ -3,36 +3,10 @@ import React from 'react';
 
 import styles from './VideoBackground.module.css';
 
-// export const VideoBackground = () => {
-//     return (
-//         <div className={styles.videoBackground}>
-//             <ReactPlayer url='/videos/sunSee.mp4' className={styles.video}  playing={true} loop={true} muted={true} >
-                
-               
-//             </ReactPlayer>
-//             { <style jsx>{`
-//                 .video-background {
-//                     position: fixed;
-//                     top: 0;
-//                     left: 0;
-//                     width: 100%;
-//                     height: 100%;
-//                     overflow: hidden;
-//                     z-index: -1;
-//                 }
 
-//                 video {
-//                     width: 100%;
-//                     height: 100%;
-//                     object-fit: cover;
-//                 }
-//             `}</style> }
-//         </div>
-//     );
-// };
 export const VideoBackground =()=>{
     return(
-        <div className={styles.VideoBackground}>
+        <div>
             <video src={require('../../../public/videos/video-background.mp4')} autoPlay muted loop className={styles.video}/>
         </div>
     )
