a25326e3bed11767ecc40f681652a91191bb5fce
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-03 21:39:52 +0200

    src video changed

--- src/components/VideoBackground/VideoBackground.js
+++ src/components/VideoBackground/VideoBackground.js
@@ -6,7 +6,7 @@ import styles from './VideoBackground.module.css';
 export const VideoBackground =()=>{
     return(
         <div>
-            <video src={require('../../assets/videos/video-background.mp4')} autoPlay muted loop className={styles.video}/>
+            <video src={require('../../../public/assets/videos/video-background.mp4')} autoPlay muted loop className={styles.video}/>
         </div>
     )
 }
