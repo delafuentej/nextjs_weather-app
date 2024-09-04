54633fbbfd0478f8a83b3579687f2aef3b7bd46a
Jesús de la Fuente <jfuentelucas@gmail.com>
2024-09-04 15:47:08 +0200

    video src

--- src/components/VideoBackground/VideoBackground.js
+++ src/components/VideoBackground/VideoBackground.js
@@ -6,6 +6,7 @@ import styles from './VideoBackground.module.css';
 export const VideoBackground =()=>{
     return(
         <div>
+            {/* video src */}
             <video src={require('../../../public/videos/video-background.mp4')} autoPlay muted loop className={styles.video}/>
         </div>
     )
